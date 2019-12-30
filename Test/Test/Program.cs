75ac80fd41de227905af8594f152d96446c805f4
Danyil Korotych <dankor1498@gmail.com>
2019-12-30 18:57:22 +0200

    Added a task about delegates.

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -6,60 +6,39 @@ using System.Threading.Tasks;
 using System.Collections;
 
 
+
 namespace Test
 {
     class Program
     {
+        delegate int MyDelegate(int a, int b);
+
         static int Sum(int a, int b)
         {
-            if (b - a == 1)
-                return b + a;
-            return b + Sum(a, b - 1);
+            return a + b;
         }
 
-        static int Rang(int number, int k)
+        static int Minus(int a, int b)
         {
-            if (k == 1)
-                return number;
-            return number * Rang(number, k - 1);
+            return a - b;
         }
 
-        static int Fibonachi(int number)
+        static int Mult(int a, int b)
         {
-            if (number == 0)
-                return 1;
-            if (number == 1)
-                return 2;
-            return Fibonachi(number - 1) + Fibonachi(number - 2);
+            return a * b;
         }
-
-        static int Factorial(int k)
+        static void Main()
         {
-            if (k == 1)
-                return k;
-            return k * Factorial(k - 1);
-        }
+            MyDelegate myDelegate = new MyDelegate(Sum);
+            myDelegate += Minus;
+            myDelegate += Mult;
 
-        [Flags]
-        enum Color : byte
-        {
-            Red = 1,
-            Green = 2,
-            Blue = 4
-        }
-
-        public static void Main(string[] args)
-        {
-            Console.WriteLine(Sum(4, 6));
-            Console.WriteLine(Rang(2, 1));
-            for (int i = 0; i < 10; i++)
+            Delegate[] delegates = myDelegate.GetInvocationList();
+            for (int i = 0; i < delegates.Length; i++)
             {
-                Console.Write(Fibonachi(i) + " ");
+                Console.WriteLine("№" + (i + 1) + " returned: " + 
+                    ((MyDelegate)delegates[i])(6, 5));
             }
-            Console.WriteLine();
-            Console.WriteLine(Factorial(5));
-            Color color = Color.Red | Color.Green | Color.Blue;
-            Console.WriteLine(color);
         }
     }
 }
