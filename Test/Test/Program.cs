c7a321e8e3becd244005dfe72da2d94b29a1dab5
Danyil Korotych <dankor1498@gmail.com>
2020-01-11 21:09:03 +0200

    Added a task about cubes.

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -10,84 +10,15 @@ namespace Test
 {
     class Program
     {
-        class BinaryNode<T> where T: IComparable
+        static int ReturnCubes(int N)
         {
-            T Name;
-            string Side;
-            public BinaryNode<T> LeftChild;
-            public BinaryNode<T> RightChild;
-            public BinaryNode(T name)
-            {
-                Name = name;
-                Side = "Top";
-            }
-
-            static public void TraversePreorder(BinaryNode<T> node)
-            {
-                Console.WriteLine(node.Side + " - " + node.Name);
-                if (node.LeftChild != null) TraversePreorder(node.LeftChild);
-                if (node.RightChild != null) TraversePreorder(node.RightChild);
-            }
-
-            static public void TraverseDepthFirst(BinaryNode<T> node)
-            {
-                Queue<BinaryNode<T>> binaryNodes = new Queue<BinaryNode<T>>();
-                binaryNodes.Enqueue(node);
-                while(binaryNodes.Count != 0)
-                {
-                    BinaryNode<T> binary = binaryNodes.Dequeue();
-                    Console.WriteLine(node.Side + " - " + node.Name);
-                    if (node.LeftChild != null) binaryNodes.Enqueue(node.LeftChild);
-                    if (node.RightChild != null) binaryNodes.Enqueue(node.RightChild);
-                }
-            }
-
-            public void AddNode(T value)
-            {
-                if (value.CompareTo(Name) < 0)
-                {
-                    if (LeftChild == null)
-                    {
-                        LeftChild = new BinaryNode<T>(value);
-                        LeftChild.Side = "Left";
-                    }
-                    else
-                    {
-                        LeftChild.AddNode(value);
-                    }
-                }
-                else
-                {
-                    if (RightChild == null)
-                    {
-                        RightChild = new BinaryNode<T>(value);
-                        RightChild.Side = "Right";
-                    }
-                    else
-                    {
-                        RightChild.AddNode(value);
-                    }
-                }
-            }
-
-
+            if (N == 1) return 1;
+            return ((N * (N + 1)) / 2) + ReturnCubes(N - 1);
         }
 
         static void Main(string[] args)
         {
-            BinaryNode<int> binary = new BinaryNode<int>(0);
-            Random random = new Random();
-            int value;
-            for (int i = 0; i < 15; i++)
-            {
-                value = random.Next(-10, 10);
-                Console.WriteLine(value);
-                binary.AddNode(value);
-            }
-            Console.WriteLine();
-            BinaryNode<int>.TraversePreorder(binary);
-            Console.WriteLine();
-            BinaryNode<int>.TraversePreorder(binary);
+            Console.WriteLine(ReturnCubes(3));
         }
 
     }
