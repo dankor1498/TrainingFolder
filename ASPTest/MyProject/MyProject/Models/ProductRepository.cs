cc9f59ee8312c303b11bfed9ed531d217cca5960
Danyil Korotych <dankor1498@gmail.com>
2020-01-16 21:44:57 +0200

    Commit 16/1/2020

---
+++ ASPTest/MyProject/MyProject/Models/ProductRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyProject.Models
+{
+    public class ProductRepository
+    {
+        private List<Product> products;
+        
+        public ProductRepository()
+        {
+            products = new List<Product>()
+            {
+                new Product{ Name = "BMW", Price = 850M },
+                new Product{ Name = "Toyota", Price = 350M },
+                new Product{ Name = "Audi", Price = 300M },
+                new Product{ Name = "Land Rover", Price = 950M },
+                new Product{ Name = "Lada", Price = 600M }
+            };
+        }
+
+        public IEnumerable<Product> GetProducts => products;
+    }
+}
--- ASPTest/Razor/Razor/Models/Product.cs
+++ ASPTest/Razor/Razor/Models/Product.cs
@@ -7,10 +7,7 @@ namespace Razor.Models
 {
     public class Product
     {
-        public int ProductID { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
         public decimal Price { get; set; }
-        public string Category { set; get; }
     }
 }
