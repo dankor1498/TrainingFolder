8b09b9323cd7b0e72786f3a31aa3b83f5a55d628
Danyil Korotych <dankor1498@gmail.com>
2020-01-19 21:44:57 +0200

    19/1/2020

---
+++ Queue/Queue/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>();
+            for (int i = 0; i < 15; i++)
+            {
+                queue.Enqueue(i);
+            }
+            queue.Dequeue();
+
+            foreach (var item in queue.PrintQueue())
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            BankQueue<int> bankQueue = new BankQueue<int>(2, 5, new int[] { 1, 3 }, new int[] { 2, 7 });
+        }
+    }
+}
---
+++ Queue/Queue/Queue.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Queue
+{
+    class Item<T>
+    {
+        public T Value { get; set; }
+        public Item<T> Pointer { get; set; }
+    }
+
+    class Queue<T>
+    {
+        Item<T> head;
+        Item<T> data;
+        Item<T> prev;
+
+        public virtual void Enqueue(T value)
+        {
+            data = new Item<T>() { Value = value };
+            if (head == null)
+            {
+                head = data;
+                prev = new Item<T>();
+                prev = data;
+                return;
+            }
+            prev.Pointer = data;
+            prev = data;
+        }
+
+        public bool IsEmpty() => head == null;
+
+        public virtual T Dequeue()
+        {
+            if(head == null)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            T value = head.Value;
+            head = head.Pointer;
+            return value;
+        }
+
+        public virtual IEnumerable<T> PrintQueue()
+        {
+            Item<T> item = head;
+            while(item != null)
+            {
+                yield return item.Value;
+                item = item.Pointer;
+            }
+        }
+    }
+
+    class BankQueue<T>
+    {
+        public int Teller { get; private set; }
+        public int Speed { get; private set; }
+
+        int[] arrivalMins = new int[2];
+        int[] duration = new int[2];
+
+        Queue<T> queue = new Queue<T>();
+
+        public BankQueue(int teller, int speed, int[]arrival, int[] duration)
+        {
+            Teller = teller;
+            Speed = speed;
+            arrivalMins[0] = arrival[0];
+            arrival.CopyTo(arrivalMins, 0);
+            duration.CopyTo(this.duration, 0);
+        }
+
+        public void StartBankQueue()
+        {
+            Random random = new Random();
+            
+            while (true)
+            {
+                //if (queue.IsEmpty() || )
+            }
+        }
+
+    }
+}
