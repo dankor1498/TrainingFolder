e148ba2d867ce280a624c15a7aef73da176cbf78
Danyil Korotych <dankor1498@gmail.com>
2020-06-15 11:37:00 +0300

    Add new task Calories

---
+++ AMC Tutor/Calories/Calories/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calories
+{
+    class CaloriesPerGram
+    {
+        const double protein = 4.1;
+        const double fats = 9.3;
+        const double carbohydrates = 4.1;
+
+        double proteinG;
+        double fatsG;
+        double carbohydratesG;
+
+        public int ID
+        {
+            get;
+            private set;
+        }
+
+        public CaloriesPerGram(int id, double p, double f, double c)
+        {
+            ID = id;
+            proteinG = p;
+            fatsG = f;
+            carbohydratesG = c;
+        }
+
+        public double CaloryGram
+        {
+            get
+            {
+                return proteinG * protein + fatsG * fats + carbohydratesG * carbohydrates;
+            }
+        }
+    }
+
+    class Program
+    {
+        class Food
+        {
+            public int ID { get; set; }
+            public double Grams { get; set; }
+
+            public Food(int id, double grams)
+            {
+                ID = id;
+                Grams = grams;
+            }
+        }
+
+        static void ReadArray(string input, List<CaloriesPerGram> c, List<Food> f)
+        {
+            string[] inputs = input.Split();
+
+            int i = 0;
+            for( ; i < inputs.Length; i++)
+            {
+                if (inputs[i] == "-1")
+                {
+                    i++;
+                    break;
+                }
+                c.Add(new CaloriesPerGram(int.Parse(inputs[i++]), 
+                    double.Parse(inputs[i++]), 
+                    double.Parse(inputs[i++]), 
+                    double.Parse(inputs[i])));
+            }
+
+            for (; i < inputs.Length; i++)
+            {
+                if (inputs[i] == "-1")
+                {
+                    break;
+                }
+                f.Add(new Food(int.Parse(inputs[i]), double.Parse(inputs[++i])));                   
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            List<CaloriesPerGram> caloriesPerGrams = new List<CaloriesPerGram>();
+            List<Food> food = new List<Food>();            
+            ReadArray(Console.ReadLine(), caloriesPerGrams, food);
+
+            double sumCalories = 0.0;
+
+            foreach(var p in food)
+            {
+                for(int i = 0; i < caloriesPerGrams.Count; i++)
+                {
+                    if(p.ID == caloriesPerGrams[i].ID)
+                    {
+                        sumCalories += caloriesPerGrams[i].CaloryGram * p.Grams;
+                        break;
+                    }
+                }
+            }
+            Console.Write($"{sumCalories:f2}");
+        }
+    }
+}
---
+++ AMC Tutor/Calories/README.md
@@ -0,0 +1,25 @@
+# Calories
+Develop a program to calculate the total number of kilocalories (kcal) in food consumed.
+A piece of food consists of:
+
+  ·Protein, 4.1 kcal/g
+
+  ·Fats, 9.3 kcal/g
+
+  ·Carbohydrates, 4.1 kcal/g
+
+Input:
+1. An array of integers, where consecutive groups of four numbers contain the information about food products:
+  1.1 Unique ID.
+  1.2 Protein content per one gram.
+  1.3 Fat content per one gram.
+  1.4 Carbohydrate content per one gram.
+2. Termination number -1.
+3. An array of integers, where consecutive groups of two numbers contain the information about food products consumed:
+  3.1 ID.
+  3.2 Amount in grams.
+Note: If there are several entries for the same product, then corresponding amounts are summed for that product.
+4. Termination number -1.
+
+Output:
+A real number, the total amount of kilocalories in the food consumed.
