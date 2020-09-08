f6819d195d884b9bfd682b9200ffd144bfd067f4
Danyil Korotych <dankor1498@gmail.com>
2020-09-08 22:06:18 +0300

    Add three tasks

--- Certification/Program.cs
+++ Certification/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Certification
 {
@@ -7,12 +8,12 @@ namespace Certification
     {
         static void Main(string[] args)
         {
-            //First task
+            // First task
             Console.WriteLine(new FirstTask("abcd", "dcba").IsReverse());
             Console.WriteLine(new FirstTask("abcd", "aaaa").IsReverse());
             Console.WriteLine();
 
-            //Second task
+            // Second task
             Dictionary<Person, string> dict = new Dictionary<Person, string>();
             dict.Add(new Person("Anna", 20), "Lviv" );
             dict.Add(new Person("Oleg", 21), "Dnipro");
@@ -27,6 +28,52 @@ namespace Certification
             {
                 Console.WriteLine($"Key: {item.Key.Name}, {item.Key.Age} Value: {item.Value}");
             }
+            Console.WriteLine();
+
+            // Third task
+            foreach (var word in WordsStartsWithA("aaa ABBB AbbB SHY Asa AWER"))
+            {
+                Console.WriteLine(word);
+            }
+            Console.WriteLine();
+
+            // Fourth task
+            foreach (var word in WordsWithoutFirst("aaa ABBB AbbB SHY Asa AWER"))
+            {
+                Console.WriteLine(word);
+            }
+            Console.WriteLine();
+
+            // Fifth task
+            foreach (var word in WordsStartsWithUpper("aaa ABBB AbbB SHY Asa AWER"))
+            {
+                Console.WriteLine(word);
+            }
+        }
+
+        static List<string> WordsStartsWithA(string sentence)
+        {
+            return sentence
+                .Split()
+                .Where(el => el.All(Char.IsUpper))
+                .Where(el => el.StartsWith('A'))
+                .ToList();
+        }
+
+        static List<string> WordsStartsWithUpper(string sentence)
+        {
+            return sentence
+                .Split()
+                .Where(el => Char.IsUpper(el.First()))
+                .ToList();
+        }
+
+        static List<string> WordsWithoutFirst(string sentence)
+        {
+            return sentence
+                .Split()
+                .Select(item => item.Remove(0,1))
+                .ToList();
         }
     }
 }
