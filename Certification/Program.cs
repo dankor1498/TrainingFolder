35ba9c49fa64ab05ab4d406c88d64eafc4272db9
Danyil Korotych <dankor1498@gmail.com>
2020-09-09 20:18:04 +0300

    Add new tasks

--- Certification/Program.cs
+++ Certification/Program.cs
@@ -49,6 +49,89 @@ namespace Certification
             {
                 Console.WriteLine(word);
             }
+            Console.WriteLine();
+
+            // Sixth task
+            Console.WriteLine(IsReverse(new[] {"a", "b", "c", "d"}, new[] {"d", "c", "b", "a"}));
+            Console.WriteLine(IsReverse(new[] {"a", "b", "c", "d"}, new[] {"a", "b", "c", "d"}));
+            Console.WriteLine();
+
+            // Seventh task
+            Console.WriteLine(SkippedNumber(new[] {0, 1, 2, 4, 5}));
+            Console.WriteLine();
+
+            // Eighth task
+            Console.WriteLine(CountEmail(new List<string>
+            {
+                "email_1@gmail.com",
+                "email_1@gmail.com",
+                "email_2@gmail.com",
+                "email_3@gmail.com",
+                "email_3@gmail.com"
+            }));
+            Console.WriteLine();
+
+            // Ninth task
+            foreach (var item in RebuildFunc(new[] {1, 2, 3, 4}, new[] {7, 7, 7}, 3))
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public static int[] RebuildFunc(int[] arr1, int[] arr2, int position)
+        {
+            int[] arr = new int[arr1.Length + arr2.Length];
+            int i = 0;
+            for (; i < position; i++)
+                arr[i] = arr1[i];
+            for (int j = 0; j < arr2.Length; j++, i++)
+                arr[i] = arr2[j];
+            for (int j = position; j < arr1.Length; j++, i++)
+                arr[i] = arr1[j];
+            return arr;
+        }
+
+        static int CountEmail(List<string> emails)
+        {
+            var res = from email in emails
+                group email by email;
+            int count = 0;
+            foreach (var r in res)
+            {
+                if (r.Count() > 1)
+                    count += r.Count();
+            }
+
+            return count;
+        }
+
+        static int SkippedNumber(int[] arr)
+        {
+            //return (arr[^1] + 1) * arr[^1] / 2 -  arr.Sum();
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] + 1 != arr[i + 1])
+                    return arr[i] + 1;
+            }
+
+            return 0;
+        }
+         
+        static bool IsReverse(string[] arr1, string[] arr2)
+        {
+            if (arr1.Length == arr2.Length)
+            {
+                for (int i = 0, j = arr1.Length - 1; i < arr1.Length; i++, j--)
+                {
+                    if (arr1[i] != arr2[j])
+                        return false;
+                }
+
+                return true;
+            }
+
+            return false;
         }
 
         static List<string> WordsStartsWithA(string sentence)
